0e5181f65bd7747081977327e9b5b5318532cf88
Erik <erik.nesvold@gmail.com>
2018-11-06 10:23:36 +0100

    Update deltas_wgan_keras.py

--- deltas_wgan_keras.py
+++ deltas_wgan_keras.py
@@ -196,7 +196,7 @@ def load_file(fname):
 
  # Split into train and test data for GAN 
 def build_dataset(X, nx, ny, n_test = 0):
-  
+
 	m = X.shape[0]
 	print("Number of images in dataset: " + str(m) )
 
@@ -205,14 +205,15 @@ def build_dataset(X, nx, ny, n_test = 0):
 
 	# Random permutation of samples
 	p = np.random.permutation(m)
-	X = X[:,p]
+	X = X[p,:]
 	Y = Y[p]
 
 	# Reshape X and crop to 96x96 pixels
 	X_new = np.zeros((m,nx,ny))
+
 	for i in range(m):
-	Xtemp = np.reshape(X[:,i],(101,101))
-	X_new[i,:,:] = Xtemp[2:98,2:98]
+		Xtemp = np.reshape(X[:,i],(101,101))
+		X_new[i,:,:] = Xtemp[2:98,2:98]
 
 	X_train = X_new[0:m-n_test,:,:]
 	Y_train = Y[0:m-n_test]
