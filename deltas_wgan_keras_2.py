e9787245ac0bf03d3b79d997c6d758a5efce8bc1
Erik <erik.nesvold@gmail.com>
2018-11-06 12:03:18 +0100

    Update deltas_wgan_keras_2.py

--- deltas_wgan_keras_2.py
+++ deltas_wgan_keras_2.py
@@ -164,7 +164,7 @@ def train(epochs=1, batchSize=128):
     print('Batches per epoch:', batchCount)
 
     for e in xrange(1, epochs+1):
-        print '-'*15, 'Epoch %d' % e, '-'*15
+        print ('-'*15, 'Epoch %d' % e, '-'*15)
         for _ in tqdm(xrange(batchCount)):
             # Get a random set of input noise and images
             noise = np.random.normal(0, 1, size=[batchSize, randomDim])
