247a36884e2b4bd0450259c688bd97e4637081f6
Erik <erik.nesvold@gmail.com>
2018-11-08 15:05:27 +0100

    Update deltas_wgan_keras.py

--- deltas_wgan_keras.py
+++ deltas_wgan_keras.py
@@ -274,4 +274,4 @@ def build_dataset(X, nx, ny, n_test = 0):
 
 if __name__ == '__main__':
 	wgan = wGAN()
-	wgan.trainGAN(epochs = 5000, batch_size = 128, sample_interval = 50)
+	wgan.trainGAN(epochs = 500, batch_size = 128, sample_interval = 10)
