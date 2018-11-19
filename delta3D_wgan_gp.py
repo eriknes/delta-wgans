0319fb55853d1934f875cde27ebef3e2b58b4eb6
Erik <erik.nesvold@gmail.com>
2018-11-19 19:47:43 +0100

    Update delta3D_wgan_gp.py

--- delta3D_wgan_gp.py
+++ delta3D_wgan_gp.py
@@ -141,16 +141,16 @@ class wGAN():
     def buildGenerator(self):
 
         generator = Sequential()
-        generator.add(Dense(128*12*12*2, input_dim=self.latent_dim, 
+        generator.add(Dense(64*12*12*2, input_dim=self.latent_dim, 
             kernel_initializer=initializers.RandomNormal(stddev=0.02)))
         generator.add(Activation("relu"))
         #generator.add(Dropout(0.2))
         generator.add(Reshape((128, 12, 12, 2)))
         generator.add(UpSampling3D(size=(2,2,2)))
-        generator.add(Conv3D(128, kernel_size=(5, 5, 3), padding='same'))
+        generator.add(Conv3D(64, kernel_size=(5, 5, 3), padding='same'))
         generator.add(Activation("relu"))
         generator.add(UpSampling3D(size=(2, 2, 2)))
-        generator.add(Conv3D(128, kernel_size=(5, 5, 3), padding='same'))
+        generator.add(Conv3D(64, kernel_size=(5, 5, 3), padding='same'))
         generator.add(Activation("relu"))
         generator.add(UpSampling3D(size=(2, 2, 2)))
         generator.add(Conv3D(self.nchan, kernel_size=(5, 5, 3), padding='same', activation='sigmoid'))
@@ -162,16 +162,16 @@ class wGAN():
 
         discriminator = Sequential()
 
-        discriminator.add(Conv3D(128, kernel_size=(5,5,3), strides=(2,2,2), input_shape=self.image_dimensions, 
+        discriminator.add(Conv3D(64, kernel_size=(5,5,3), strides=(2,2,2), input_shape=self.image_dimensions, 
             padding="same", kernel_initializer=initializers.RandomNormal(stddev=0.02)))
         discriminator.add(LeakyReLU(.2))
         discriminator.add(Dropout(0.3))
 
-        discriminator.add(Conv3D(128, kernel_size=(5,5,3), strides=(2,2,2), padding="same"))
+        discriminator.add(Conv3D(64, kernel_size=(5,5,3), strides=(2,2,2), padding="same"))
         discriminator.add(LeakyReLU(.2))
         discriminator.add(Dropout(0.3))
 
-        discriminator.add(Conv3D(128, kernel_size=(5,5,3), strides=(2,2,2), padding="same"))
+        discriminator.add(Conv3D(64, kernel_size=(5,5,3), strides=(2,2,2), padding="same"))
         discriminator.add(LeakyReLU(.2))
         discriminator.add(Dropout(0.3))
         discriminator.add(Flatten())
