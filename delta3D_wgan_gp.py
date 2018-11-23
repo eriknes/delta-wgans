271947ff8154838b82d93d9259f8cc99b981987d
Erik <erik.nesvold@gmail.com>
2018-11-23 09:35:45 +0100

    Update delta3D_wgan_gp.py

--- delta3D_wgan_gp.py
+++ delta3D_wgan_gp.py
@@ -153,7 +153,7 @@ class wGAN():
         generator.add(Conv3D(128, kernel_size=(5, 5, 5), padding='same'))
         generator.add(Activation("relu"))
         generator.add(UpSampling3D(size=(2, 2, 2)))
-        generator.add(Conv3D(256, kernel_size=(5, 5, 5), padding='same'))
+        generator.add(Conv3D(128, kernel_size=(5, 5, 5), padding='same'))
         generator.add(Activation("relu"))
         generator.add(Conv3D(self.nchan, kernel_size=(5, 5, 5), padding='same', activation='sigmoid'))
         generator.summary()
