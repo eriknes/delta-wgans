0991a1a5d923a52c15987b59c757cdd786507ce2
Erik <erik.nesvold@gmail.com>
2018-11-27 11:25:38 +0100

    Update delta3D_wgan_gp.py

--- delta3D_wgan_gp.py
+++ delta3D_wgan_gp.py
@@ -144,10 +144,11 @@ class wGAN():
         #generator.add(Dense(128, input_dim=self.latent_dim, 
         #    kernel_initializer=initializers.RandomNormal(stddev=0.02)))
         #generator.add(Activation("relu"))
-        generator.add(Dense(16*12*12*2, input_dim=self.latent_dim, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
-        generator.add(Reshape((16, 12, 12, 2)))
-        generator.add(UpSampling3D(size=(1, 1, 2)))
+        generator.add(Dense(16*12*12*4, input_dim=self.latent_dim, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
+        generator.add(Reshape((16, 12, 12, 4)))
+
         generator.add(Activation("relu"))
+        generator.add(UpSampling3D(size=(2, 2, 2)))
         #generator.add(Dropout(0.2))
         
         generator.add(Conv3D(128, kernel_size=(9, 9, 3), padding='same'))
