84c2e1f3ecde3bc44c15c84234803e4da4127bcb
Erik <erik.nesvold@gmail.com>
2018-12-05 22:26:41 +0100

    Update delta3D_wgan_gp.py

--- delta3D_wgan_gp.py
+++ delta3D_wgan_gp.py
@@ -144,19 +144,21 @@ class wGAN():
         #generator.add(Dense(128, input_dim=self.latent_dim, 
         #    kernel_initializer=initializers.RandomNormal(stddev=0.02)))
         #generator.add(Activation("relu"))
-        generator.add(Dense(256*8*8*2, input_dim=self.latent_dim, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
-        generator.add(Reshape((256, 8, 8, 2)))
+        generator.add(Dense(512*8*8*2, input_dim=self.latent_dim, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
+        generator.add(Reshape((512, 8, 8, 2)))
 
         generator.add(Activation("relu"))
         generator.add(UpSampling3D(size=(2, 2, 2)))
         #generator.add(Dropout(0.2))
         
-        generator.add(Conv3D(128, kernel_size=(7, 7, 5), padding='same'))
+        generator.add(Conv3D(128, kernel_size=(5, 5, 5), padding='same'))
         generator.add(Activation("relu"))
         generator.add(UpSampling3D(size=(2, 2, 2)))
         generator.add(Conv3D(64, kernel_size=(7, 7, 5), padding='same'))
         generator.add(Activation("relu"))
         generator.add(UpSampling3D(size=(2, 2, 2)))
+        generator.add(Conv3D(64, kernel_size=(7, 7, 5), padding='same'))
+        generator.add(Activation("relu"))
         #generator.add(UpSampling3D(size=(2, 2, 2)))
         #generator.add(Conv3D(78, kernel_size=(5, 5, 5), padding='same'))
         #generator.add(Activation("relu"))
@@ -205,7 +207,7 @@ class wGAN():
         dummy_y     = np.zeros((self.batch_size, 1), dtype=np.float32)
 
 
-        eps             = .5
+        eps             = .2
         randomDim       = 12
 
         #batch_count = int(X_train.shape[0] / (self.batch_size * N_CRITIC_ITER))
