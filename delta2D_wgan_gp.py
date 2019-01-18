1f5a5df993cdcb327ee375f7cdeca98dcb9a975b
Erik <erik.nesvold@gmail.com>
2019-01-18 12:50:39 +0100

    Update delta2D_wgan_gp.py

--- delta2D_wgan_gp.py
+++ delta2D_wgan_gp.py
@@ -131,27 +131,27 @@ class wGAN():
     def buildGenerator(self):
 
         generator = Sequential()
-        generator.add(Dense(256*12*12, input_dim=self.latent_dim, 
+        generator.add(Dense(64*12*12, input_dim=self.latent_dim, 
             kernel_initializer=initializers.RandomNormal(stddev=0.02)))
         #generator.add(LeakyReLU(.2))
-        generator.add(Activation("relu"))
+        #generator.add(Activation("relu"))
         #generator.add(Dropout(0.2))
-        generator.add(Reshape((256, 12, 12)))
+        generator.add(Reshape((64, 12, 12)))
         generator.add(Activation("relu"))
 
         generator.add(UpSampling2D(size=(2, 2)))
-        generator.add(Conv2D(128, kernel_size=(7,7), padding='same'))
+        generator.add(Conv2D(32, kernel_size=(5,5), padding='same'))
         generator.add(Activation("relu"))
 
         generator.add(UpSampling2D(size=(2, 2)))
-        generator.add(Conv2D(64, kernel_size=(7, 7), padding='same'))
+        generator.add(Conv2D(16, kernel_size=(5, 5), padding='same'))
         generator.add(Activation("relu"))
 
         generator.add(UpSampling2D(size=(2, 2)))
-        generator.add(Conv2D(64, kernel_size=(7, 7), padding='same'))
+        generator.add(Conv2D(8, kernel_size=(5, 5), padding='same'))
         generator.add(Activation("relu"))
         
-        generator.add(Conv2D(self.nchan, kernel_size=(7, 7), padding='same', 
+        generator.add(Conv2D(self.nchan, kernel_size=(5, 5), padding='same', 
             activation='sigmoid', kernel_initializer=initializers.RandomNormal(stddev=0.02)))
         generator.summary()
 
@@ -162,24 +162,24 @@ class wGAN():
 
         discriminator = Sequential()
 
-        discriminator.add(Convolution2D(64, kernel_size=(7,7), strides=(2,2), input_shape=self.image_dimensions, 
+        discriminator.add(Convolution2D(8, kernel_size=(5,5), strides=(2,2), input_shape=self.image_dimensions, 
             padding="same", kernel_initializer=initializers.RandomNormal(stddev=0.02)))
         discriminator.add(LeakyReLU(.2))
         discriminator.add(Dropout(0.3))
 
-        discriminator.add(Convolution2D(64, kernel_size=(7,7), strides=(2,2), padding="same"))
+        discriminator.add(Convolution2D(16, kernel_size=(5,5), strides=(2,2), padding="same"))
         #discriminator.add(ZeroPadding2D(padding=((0,1),(0,1))))
         #discriminator.add(BatchNormalization(momentum=0.7))
         discriminator.add(LeakyReLU(.2))
         discriminator.add(Dropout(0.3))
 
-        discriminator.add(Convolution2D(128, kernel_size=(7,7), strides=(2,2), padding="same"))
+        discriminator.add(Convolution2D(32, kernel_size=(5,5), strides=(2,2), padding="same"))
         #discriminator.add(ZeroPadding2D(padding=((0,1),(0,1))))
         #discriminator.add(BatchNormalization(momentum=0.7))
         discriminator.add(LeakyReLU(.2))
         discriminator.add(Dropout(0.3))
 
-        discriminator.add(Convolution2D(256, kernel_size=(7,7), strides=(2,2), padding="same"))
+        discriminator.add(Convolution2D(64, kernel_size=(5,5), strides=(2,2), padding="same"))
         #discriminator.add(BatchNormalization(momentum=0.7))
         discriminator.add(LeakyReLU(.2))
         discriminator.add(Dropout(0.3))
@@ -327,8 +327,8 @@ class wGAN():
 
     # Save the generator and discriminator networks (and weights) for later use
     def saveModels(self, epoch):
-        self.generator.save('models/wgan_gen_ep_%d.h5' % epoch)
-        #self.discriminator.save('models/wgan_discriminator_epoch_%d.h5' % epoch)
+        self.generator.save('models/gen_epoch_%d.h5' % epoch)
+        self.discriminator.save('models/disc_epoch_%d.h5' % epoch)
 
 # Read csv file
 def load_file(fname):
